51cbbd4aa9f48ecdfded26e7c11b858e67b384be
Blecki <jm@omnisu.com>
2014-04-24 21:15:56 -0400

    Add floor, ceil, abs math functions

---
+++ EtcScriptLib/StandardLibrary/MathFunctions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EtcScriptLib
+{
+	public partial class StandardLibrary
+	{
+		public static void MathFunctions(Environment Environment)
+		{
+			Environment.AddSystemMacro("floor (n:number) : number",
+				(c, a) => { return (float)(Math.Floor(Convert.ToSingle(a[0]))); });
+			Environment.AddSystemMacro("ceil (n:number) : number",
+				(c, a) => { return (float)(Math.Ceiling(Convert.ToSingle(a[0]))); });
+			Environment.AddSystemMacro("abs (n:number) : number",
+				(c, a) => { return (float)(Math.Abs(Convert.ToSingle(a[0]))); });
+		}
+	}
+}
